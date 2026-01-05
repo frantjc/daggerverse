17e7dc4bb222388e5891b85b0ae5219cf54d521b
Alex <logsdonalex11@gmail.com>
2026-01-05 00:16:48 -0500

    chore(gh): flatten module

--- gh/main.go
+++ gh/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/frantjc/daggerverse/gh/internal/dagger"
 )
 
@@ -12,6 +15,50 @@ func New(githubToken *dagger.Secret) *Gh {
 	return &Gh{githubToken: githubToken}
 }
 
-func (m *Gh) Release() *dagger.Release {
-	return dag.Release(m.githubToken)
+type Release struct {
+	gh *Gh
+}
+
+func (m *Gh) Release() *Release {
+	return &Release{gh: m}
+}
+
+func (m *Release) Create(ctx context.Context,
+	tag,
+	repo string,
+	// +optional
+	assets []*dagger.File,
+) error {
+	repoArg := fmt.Sprintf("-R=%s", repo)
+
+	release := dag.Wolfi().
+		Container(dagger.WolfiContainerOpts{
+			Packages: []string{"gh"},
+		}).
+		WithSecretVariable("GITHUB_TOKEN", m.gh.githubToken).
+		WithExec([]string{"gh", "release", repoArg, "create", tag, "--generate-notes", "--draft"})
+
+	for _, asset := range assets {
+		file, err := asset.Name(ctx)
+		if err != nil {
+			return err
+		}
+
+		release = release.
+			WithFile(
+				file,
+				asset,
+			).
+			WithExec([]string{
+				"gh", "release", repoArg, "upload", tag, file,
+			})
+	}
+
+	if _, err := release.
+		WithExec([]string{"gh", "release", repoArg, "edit", tag, "--latest", "--draft=false"}).
+		Sync(ctx); err != nil {
+		return err
+	}
+
+	return nil
 }
